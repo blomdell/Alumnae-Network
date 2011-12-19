76ee743aecd5aa39678cd09a9116f4a448aab879
Tor Blomdell <tor.blomdell@gmail.com>
2011-12-18 21:35:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlumnaeNetwork::Application.config.secret_token = 'fac30a608fb8315b247f7567efb1fdc07522117c211bc8e95692b0252fb2080f28fe472c5b4946d8423d8b7539be6fb1b7250e68c06f8a5be3fc0b056dcefe2c'
